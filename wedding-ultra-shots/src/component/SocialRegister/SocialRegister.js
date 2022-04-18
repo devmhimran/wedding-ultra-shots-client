db4195ae1f5cbb5b5a8d106bb850bd612043a23d
Mahmud Hasan Imran <mahmud.bubt.150@gmail.com>
2022-04-18 20:09:54 +0600

    Update Social Login

--- wedding-ultra-shots/src/component/SocialRegister/SocialRegister.js
+++ wedding-ultra-shots/src/component/SocialRegister/SocialRegister.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import { useSignInWithGoogle } from 'react-firebase-hooks/auth';
 import { BsGoogle } from 'react-icons/bs';
+import { useLocation, useNavigate } from 'react-router-dom';
 import auth from '../firebase.init';
 
 const SocialRegister = () => {
     const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
+    const navigate = useNavigate();
+    const location = useLocation();
+    let from = location.state?.from?.pathname || "/";
+    if (user) {
+        navigate(from, { replace: true });
+    }
     return (
         <div className="input__form">
             <button className='signin__button w-100' onClick={()=>signInWithGoogle()}><BsGoogle></BsGoogle> <span>Google</span></button>
