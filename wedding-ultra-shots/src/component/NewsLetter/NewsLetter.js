9add52c2dfd940ef1f22c3846cdce7c3ad175633
Mahmud Hasan Imran <mahmud.bubt.150@gmail.com>
2022-04-19 21:03:42 +0600

    Update Homepage

---
+++ wedding-ultra-shots/src/component/NewsLetter/NewsLetter.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const newsLetter = () => {
+    return (
+        <div className='container'>
+            <div className="subscribe__form mx-auto">
+                <h1 className='text-center'>Subscribe for latest update</h1>
+                <div className="card w-75 mx-auto">
+                    <div className="card-body p-4">
+                        <form>
+                            <div className="input__form">
+                                <small>Enter Email</small>
+                                <input type="email" placeholder='example@domain.com' required />
+                            </div>
+                            <div className="input__form">
+                                <button className='signin__button'>Sign in</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default newsLetter;
